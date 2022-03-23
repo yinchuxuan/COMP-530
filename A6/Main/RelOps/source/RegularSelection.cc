f67d00ac7d6e8d201ae2fc19468d127d8e477ebe
xxxuduo2022 <dx9@rice.edu>
2022-03-22 19:44:27 -0500

    add RegularSelection

--- A6/Main/RelOps/source/RegularSelection.cc
+++ A6/Main/RelOps/source/RegularSelection.cc
@@ -4,9 +4,38 @@
 
 #include "RegularSelection.h"
 
-RegularSelection :: RegularSelection (MyDB_TableReaderWriterPtr, MyDB_TableReaderWriterPtr,
-                string, vector <string>) {}
+RegularSelection :: RegularSelection (MyDB_TableReaderWriterPtr inputIn, MyDB_TableReaderWriterPtr outputIn,
+                string selectionPredicateIn, vector <string> projectionsIn) {
+    input = inputIn;
+    output = outputIn;
+    selectionPredicate = selectionPredicateIn;
+    projections = projectionsIn;
+}
 
-void RegularSelection :: run () {}
+void RegularSelection :: run () {
+    MyDB_RecordPtr inputRec = input->getEmptyRecord();
+    MyDB_RecordPtr outputRec = output->getEmptyRecord();
+    func inputPred = inputRec->compileComputation (selectionPredicate);
+    // and get the final set of computations that will be used to build the output record
+    vector <func> finalComputations;
+    for (string s : projections) {
+        finalComputations.push_back (combinedRec->compileComputation (s));
+    }
+    // now, iterate through the input table
+    MyDB_RecordIteratorPtr myIter = rightTable->getIterator (input);
+    while(myIter->hasNext()) {
+        myIter->getNext ();
+        // see if it is accepted by the preicate
+        if (myIter ()->toBool ()) {
+            // run all of the computations
+            int i = 0;
+            for (auto &f : finalComputations) {
+                outputRec->getAtt (i++)->set (f());
+            }
+            outputRec->recordContentHasChanged ();
+            output->append (outputRec);
+        }
+    }
+}
 
 #endif
